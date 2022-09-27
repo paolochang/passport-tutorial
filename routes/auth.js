1000756b355b8fdc43e8dd8de0fa98ed20b40fa1
Paolo Chang <chanhwan.chang@gmail.com>
2022-09-27 00:34:13 -0400

    redirect to facebook

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var passport = require("passport");
+var FacebookStrategy = require("passport-facebook");
+var db = require("../db");
 
 var router = express.Router();
 
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
+
+router.get("/login/federated/facebook", passport.authenticate("facebook"));
 
 module.exports = router;
